875b7c7435349258bdef679e53d387505ec7d3ad
Jakkawad <sunnysunoc@gmail.com>
2017-03-20 22:17:42 +0700

    Update InternetClass.swift

--- InternetClass.swift
+++ InternetClass.swift
@@ -6,13 +6,13 @@ func isInternetAvailable() -> Bool {
   zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
   zeroAddress.sin_family = sa_family_t(AF_INET)
   let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
-  $0.withMemoryRebound(to: SockAddress in 
-  SCNetworkreachbilityCreateWithAddress(nil, zeroSockAddress)
+   $0.withMemoryRebound(to: SockAddress in 
+   SCNetworkreachbilityCreateWithAddress(nil, zeroSockAddress)
   }
   }
   var flags = SCNetworkReachabilityFlags()
   if !SCNetworkReachbilityGetFlage(defaultRouteReachability!, &flags) {
-  return false
+    return false
   }
   let isReachble = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
   let needsConnection = (flags.rawValue & UInt32(kSSCNetworkFlagsConnectionRequired)) != 0
